b11f32bbc5ea2a624baa58b6c4488b6c56fce33a
zacktan5027 <zacktan5027@gmail.com>
2021-06-17 22:03:25 +0800

    remove unnessary line

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,20 +1,7 @@
-/*!
- * Start Bootstrap - SB Admin v7.0.0 (https://startbootstrap.com/template/sb-admin)
- * Copyright 2013-2021 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
- */
-//
-// Scripts
-//
-
 window.addEventListener("DOMContentLoaded", (event) => {
   // Toggle the side navigation
   const sidebarToggle = document.body.querySelector("#sidebarToggle");
   if (sidebarToggle) {
-    // Uncomment Below to persist sidebar toggle between refreshes
-    // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-    //     document.body.classList.toggle('sb-sidenav-toggled');
-    // }
     sidebarToggle.addEventListener("click", (event) => {
       event.preventDefault();
       document.body.classList.toggle("sb-sidenav-toggled");
@@ -27,9 +14,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  // Simple-DataTables
-  // https://github.com/fiduswriter/Simple-DataTables/wiki
-
   const staffList = document.getElementById("staffList");
   if (staffList) {
     new simpleDatatables.DataTable(staffList);
